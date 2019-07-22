24c3c05074bc927e6e39774c6dfc9ee9c70fd53b
@mmnogueira <messias.nogueira@gmail.com>
2019-07-21 22:59:00 -0300

    Versão inicial do projeto

---
+++ src/main/java/com/kazale/qpi/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.kazale.qpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+
+}
